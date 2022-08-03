3bad889de8a1b2b47b9d117b09b6f3459c97cd00
Lucas Almeida <lucas970997@gmail.com>
2022-08-03 11:55:23 -0300

    :lipstick: stylization done

--- SupermarketAssistent/src/pages/home/Home.tsx
+++ SupermarketAssistent/src/pages/home/Home.tsx
@@ -1,10 +1,39 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  Text,
+  View,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import {Container, AddItems} from './styles';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+const items = [
+  {item: 'Item 1', valor: 'valor1', quantidade: 'quantidade1'},
+  {item: 'Item 2', valor: 'valor2', quantidade: 'quantidade2'},
+  {item: 'Item 3', valor: 'valor3', quantidade: 'quantidade3'},
+];
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <Container>
+      <StatusBar barStyle="dark-content" backgroundColor={'#ffffff'} />
+      <ScrollView>
+        {items.map((item, index) => (
+          <View key={index} style={{flexDirection: 'row'}}>
+            <Text>{item.item}</Text>
+            <Text>{item.valor}</Text>
+            <Text>{item.quantidade}</Text>
+          </View>
+        ))}
+      </ScrollView>
+      <AddItems>
+        <TouchableOpacity>
+          <Icon name="pluscircle" size={60} color="#fff" />
+        </TouchableOpacity>
+      </AddItems>
+    </Container>
   );
 }
---
+++ SupermarketAssistent/src/pages/home/styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #ffffff;
+  align-items: center;
+`;
+export const AddItems = styled.View`
+  min-width: 100%;
+  min-height: 14%;
+  background: #868484;
+  align-items: center;
+  justify-content: center;
+`;
